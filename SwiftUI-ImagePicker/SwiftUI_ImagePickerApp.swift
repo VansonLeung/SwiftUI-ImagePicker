bf4bb8ab654fec696f97ad6589dda2fff3cabdc7
Vanson Leung <vanson@vanportdev.com>
2023-07-03 18:02:24 +0800

    Initial Commit

---
+++ SwiftUI-ImagePicker/SwiftUI_ImagePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ImagePickerApp.swift
+//  SwiftUI-ImagePicker
+//
+//  Created by Leung Yu Wing on 3/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ImagePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
